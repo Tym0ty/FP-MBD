1c7fe827c2a88d39fdf8855ba65f2b65a51c540a
Brendan Timothy Mannuel <143975786+Tym0ty@users.noreply.github.com>
2024-06-21 12:54:04 +0700

    Update Insert.sql

--- Insert.sql
+++ Insert.sql
@@ -1,1 +1,4 @@
 CREATE DATABASE IF NOT EXISTS FP_MBD;
+
+INSERT INTO FP_MBD.Alumnus (NRP, Nama, Tanggal_Lahir, Alamat, No_HP, Email, Tahun_Masuk, Tahun_Lulus, Jenis_Kelamin, ID_Pekerjaan) 
+    VALUES ('5115100001', 'John Doe', '1993-01-01', 'Jl. ABC', '08123456789', '
