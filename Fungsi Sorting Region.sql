42e45cf4a89a7f078c5b963de517d673e06ecc9e
Brendan Timothy Mannuel <143975786+Tym0ty@users.noreply.github.com>
2024-06-23 21:35:28 +0700

    Update Fungsi Sorting Region.sql

--- Fungsi Sorting Region.sql
+++ Fungsi Sorting Region.sql
@@ -1,15 +1,13 @@
-USE fp_mbd;
+-- Active: 1719153001263@@127.0.0.1@3306@fp_mbd
 
-DELIMITER $$
 
-CREATE PROCEDURE fp_mbd.get_alumnus_by_city(IN city_name VARCHAR(255))
+CREATE PROCEDURE get_alumnus_by_city(IN city_name VARCHAR(255))
 BEGIN
     SELECT *
     FROM Alumnus
     WHERE Asal_Kota = city_name;
-END$$
+END;
 
-DELIMITER ;
 
-CALL fp_mbd.get_alumnus_by_city('Banyuwangi');
+CALL get_alumnus_by_city('Banyuwangi');
 
