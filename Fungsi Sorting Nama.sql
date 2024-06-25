8d1d61763e6b9fb2e11680cda60d5303c0735e0b
Brendan Timothy Mannuel <143975786+Tym0ty@users.noreply.github.com>
2024-06-25 21:37:41 +0700

    Update Fungsi Sorting Nama.sql

--- Fungsi Sorting Nama.sql
+++ Fungsi Sorting Nama.sql
@@ -1,4 +1,4 @@
--- Active: 1719310112817@@127.0.0.1@3306@fp_mbd
+-- Active: 1719251590957@@127.0.0.1@3306@fp_mbd
 
 
 CREATE PROCEDURE get_alumnus_by_name(IN alumnus_name VARCHAR(100))
@@ -6,7 +6,7 @@ BEGIN
     SELECT *
     FROM Alumnus
     WHERE 
-        per.Nama LIKE CONCAT('%', alumnus_name, '%');
+        Alumnus.Nama LIKE CONCAT('%', alumnus_name, '%');
 END;
 
 CALL get_alumnus_by_name('a');
