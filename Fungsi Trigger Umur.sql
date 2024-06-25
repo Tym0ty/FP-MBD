533a8767da012c028fd02f18ca311c9bbed2a727
thifa <113702902+lathifash@users.noreply.github.com>
2024-06-26 01:09:46 +0700

    trigger

---
+++ Fungsi Trigger Umur.sql
@@ -0,0 +1,20 @@
+ALTER TABLE Alumnus ADD COLUMN IF NOT EXISTS Usia INT;
+
+-- Event Scheduler
+SET GLOBAL event_scheduler = ON;
+
+DROP EVENT IF EXISTS update_usia_annually;
+
+-- Buat event baru untuk memperbarui usia setiap tahun
+CREATE EVENT update_usia_annually
+ON SCHEDULE EVERY 1 YEAR
+STARTS NOW()
+DO
+BEGIN
+    UPDATE Alumnus
+    SET Usia = YEAR(CURDATE()) - YEAR(Tanggal_Lahir);
+END;
+
+-- Jalankan update usia untuk pertama kali
+UPDATE Alumnus
+SET Usia = YEAR(CURDATE()) - YEAR(Tanggal_Lahir);
