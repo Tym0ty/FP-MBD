c3bf9ce0f2c20d20e0f592b9fdd38eee6115bd3f
Brendan Timothy Mannuel <143975786+Tym0ty@users.noreply.github.com>
2024-06-25 17:34:16 +0700

    update fungsi search

---
+++ Fungsi Search.sql
@@ -0,0 +1,13 @@
+-- Active: 1719251590957@@127.0.0.1@3306@fp_mbd
+
+
+CREATE PROCEDURE get_alumnus_by_city(IN city_name VARCHAR(255))
+BEGIN
+    SELECT *
+    FROM Alumnus
+    WHERE Asal_Kota = city_name;
+END;
+
+
+CALL get_alumnus_by_city('Banyuwangi');
+
