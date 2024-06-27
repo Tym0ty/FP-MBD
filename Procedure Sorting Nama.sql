bbd66f2d041826a88fa093373ff54c2df978d600
thifa <113702902+lathifash@users.noreply.github.com>
2024-06-27 10:16:19 +0700

    Update Procedure Sorting Nama.sql

--- Procedure Sorting Nama.sql
+++ Procedure Sorting Nama.sql
@@ -1,5 +1,7 @@
 -- Active: 1719251590957@@127.0.0.1@3306@fp_mbd
 
+# Index 
+CREATE INDEX idx_nama_alumnus ON Alumnus(Nama);
 
 CREATE PROCEDURE get_alumnus_by_name(IN alumnus_name VARCHAR(100))
 BEGIN
